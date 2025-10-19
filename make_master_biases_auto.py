5b328462c468486fd0fecbe7063bcfbebcd5361d
rolandet <roland.teague@gmail.com>
2025-10-19 13:32:07 -0400

    Update make_master_biases_auto.py
    Added GNU GPL 3.0 license verbiage to align with Siril.

--- make_master_biases_auto.py
+++ make_master_biases_auto.py
@@ -2,6 +2,11 @@
 # Builds master bias frames for a NINA-style folder layout.
 # ROOT comes from Python's current working directory; camera/binning read from FITS headers.
 
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, 
+# or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# See <https://www.gnu.org/licenses/>.
+
 import os
 import shutil
 import sirilpy as s
@@ -149,3 +154,4 @@ finally:
     for d in os.listdir(OUT):
         if d.startswith("_work_"):
             shutil.rmtree(os.path.join(OUT, d), ignore_errors=True)
+
